1c2260944bc8748fac126ebe3a57098a3b592e6a
Prakash Goud <prakash.goud@morningstar.com>
2021-08-28 00:40:23 +0530

    [Prakash] - Added more nodes in binary tree

--- src/com/bridgelabz/binarysearchtree/MyBinaryTreeRunner.java
+++ src/com/bridgelabz/binarysearchtree/MyBinaryTreeRunner.java
@@ -7,6 +7,16 @@ public class MyBinaryTreeRunner {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
 		int size = myBinaryTree.size();
 		System.out.println("The size of binary tree is :- " + size);
 
